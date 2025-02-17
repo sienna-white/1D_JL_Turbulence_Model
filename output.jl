6e87c0712bed90ce2e14302a561e4f7656232e84
Sienna White <siennarwhite@gmail.com>
2025-02-16 19:27:02 -0800

    first push -- code working fairly wellgit add .

---
+++ output.jl
@@ -0,0 +1,38 @@
+# Library to deal with model output 
+output = Dict()
+times = [] 
+
+# ***********************************************************************
+function create_output_dict(M::Int, isave::Int, Vars::Array{<:String}, N::Int, output=output)
+    nvars = length(Vars)
+    n_saved_steps = div(M,isave)
+
+    for i = 1:nvars
+        println(Vars[i])
+        output[Vars[i]] = zeros(Float64, N, n_saved_steps)
+    end
+end 
+# ***********************************************************************
+
+
+# ***********************************************************************
+function save2output(time, index, varname, value, output=output)
+    output[varname][:,index] .= value
+    push!(times, time)
+end 
+# ***********************************************************************
+
+function plot_variable(varname, discretization, output=output)
+    ntimes = length(unique(times))
+    print(output)
+    print("Plotting $(ntimes) steps")
+    z = discretization["z"]
+    p = plot() 
+
+    for i in 1:(ntimes-1)
+        print(i)
+        print(output[varname][:,i])
+        plot!( output[varname][:,i], -z) # label="t = $(times[i])")
+    end
+   display(p) # plot(times, output[varname][1,:])
+end
